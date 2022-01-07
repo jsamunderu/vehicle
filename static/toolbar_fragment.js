c1124b81422a8a10858837b96d3da5db33bcc319
James Samunderu <jsamunderu@gmail.com>
2022-01-07 09:58:45 +0200

    fix the static files escaping

--- static/toolbar_fragment.js
+++ static/toolbar_fragment.js
@@ -11,8 +11,8 @@ function ready(callback) {
 }
 ready(function() {
 	var iframe = document.createElement('iframe');
-	document.getElementById(\"content\").appendChild(iframe);
-	iframe.setAttribute(\"style\",\"height:100%;width:100%;\");
+	document.getElementById("content").appendChild(iframe);
+	iframe.setAttribute("style","height:100%;width:100%;");
 	iframe.contentWindow.document.open();
 	iframe.contentWindow.document.write(`%s`);
 	iframe.contentWindow.document.close();
